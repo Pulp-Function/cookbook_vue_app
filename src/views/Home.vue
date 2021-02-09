4759689ecfd606f913793f4c67a88413f302d111
Peter Jang <peter.x.jang@gmail.com>
2021-02-08 20:19:22 -0600

    Add recipes create action (hard coded params)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <div>
+      <h1>New Recipe</h1>
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
     <h1>All recipes</h1>
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
@@ -33,6 +37,24 @@ export default {
         this.recipes = response.data;
         console.log("All recipes:", this.recipes);
       });
+    },
+    createRecipe: function() {
+      console.log("createRecipe");
+      var params = {
+        title: "example title",
+        chef: "example chef",
+        prep_time: 32,
+        ingredients: "one, two, three",
+        directions: "cook, serve",
+        image_url: "example image",
+      };
+      axios
+        .post("/api/recipes", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.recipes.push(response.data);
+        })
+        .catch(error => console.log(error.response));
     },
   },
 };
