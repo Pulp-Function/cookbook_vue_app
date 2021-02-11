5c1c66a39dc692372c15831efa83d9a8ea268a97
Peter Jang <peter.x.jang@gmail.com>
2021-02-10 19:40:31 -0600

    Add seperate recipes index page

---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>All recipes</h1>
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Chef: {{ recipe.chef }}</p>
+      <img v-bind:src="recipe.image_url" alt="" />
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  width: 250px;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      recipes: [],
+    };
+  },
+  created: function() {
+    this.indexRecipes();
+  },
+  methods: {
+    indexRecipes: function() {
+      axios.get("/api/recipes").then(response => {
+        this.recipes = response.data;
+        console.log("All recipes:", this.recipes);
+      });
+    },
+  },
+};
+</script>
