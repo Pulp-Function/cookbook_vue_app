f06b8465d1b2fce8fb11d101d103555489c94533
Peter Jang <peter.x.jang@gmail.com>
2021-03-04 18:49:55 -0600

    Change image to upload file instead of string

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -28,7 +28,7 @@
       </div>
       <div class="form-group">
         <label>Image url:</label>
-        <input type="text" class="form-control" v-model="imageUrl" />
+        <input type="file" class="form-control" v-on:change="setFile($event)" ref="fileInput" />
       </div>
       <input type="submit" class="btn btn-primary" value="Submit" />
     </form>
@@ -46,21 +46,35 @@ export default {
       prepTime: "",
       ingredients: "",
       directions: "",
-      imageUrl: "",
+      image: "",
       status: "",
       errors: [],
     };
   },
   methods: {
+    setFile: function(event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
+    },
     createRecipe: function() {
-      var params = {
-        title: this.title,
-        chef: this.chef,
-        prep_time: this.prepTime,
-        ingredients: this.ingredients,
-        directions: this.directions,
-        image_url: this.imageUrl,
-      };
+      // var params = {
+      //   title: this.title,
+      //   chef: this.chef,
+      //   prep_time: this.prepTime,
+      //   ingredients: this.ingredients,
+      //   directions: this.directions,
+      //   image_url: this.imageUrl,
+      // };
+
+      var params = new FormData();
+      params.append("title", this.title);
+      params.append("chef", this.chef);
+      params.append("prep_time", this.prepTime);
+      params.append("ingredients", this.ingredients);
+      params.append("directions", this.directions);
+      params.append("image", this.image);
+
       axios
         .post("/api/recipes", params)
         .then(response => {
